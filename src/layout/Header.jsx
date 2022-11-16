b1533282cbfe3193e8f9e5c1601cf7c061b20483
Artem <aapliusnin@gmaile.com>
2022-11-16 19:30:10 +0200

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="purple darken-3">
+        <div className="nav-wrapper">
+            <a href="#" className="brand-logo">Movies</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="#">REP</a></li>
+            </ul>
+        </div>
+    </nav>
+}
+
+export {Header}
